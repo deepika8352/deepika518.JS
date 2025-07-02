51db5483d659beb5dfee8173568366e8f10b9984
deepika8352 <deepikaagidi@gmail.com>
2025-07-02 12:51:37 +0530

    Add files via upload

---
+++ JS_SAMPLES/funexp.js
@@ -0,0 +1,12 @@
+function welcomeMessage(){
+    console.log("Welcome to MRECW");
+}
+function add(a,b){
+    return a+b;
+}
+const mul=(a,b)=>(a*b);
+console.log(mul(10,40));
+const greet=()=>("Hello");
+console.log(greet());
+// welcomeMessage();
+// console.log(add(10,20));
---
+++ JS_SAMPLES/object.js
@@ -0,0 +1,5 @@
+let user={name:"deepu",dept:"mca",age:35};
+//console.log(user);
+console.log(user.name);
+console.log(user.dept);
+console.log(user.age);
